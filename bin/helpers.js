3bd2e6908e795015547e28cc17afa3a2f40510a5
Alec Utin <alec@utin.de>
2018-02-18 13:28:08 +0100

    Rename bin directory

--- bin/helpers.js
+++
@@ -1,9 +0,0 @@
-/**
- * removes last suffix after dot
- * @param {String} name - filename
- */
-const removeFileExt = name => name.split('.').filter((f, i, a) => i !== a.length - 1).join('.');
-
-module.exports = {
-    removeFileExt
-};

ee7567a618c3144f4045f6fcbf232d685a9e7323
Alec Utin <alec@utin.de>
2018-02-18 11:04:47 +0100

    Add sample.lua to .gitignore

--- spec/8pack.spec.js
+++ spec/8pack.spec.js
@@ -1,16 +1,16 @@
-describe('CLI Tests',()=>{
-    it('should show the man page on no arguments',()=>{
-
+describe('CLI Tests', () => {
+    it('should show the man page on no arguments', () => {
+        //todo
     });
-    it('should show the man page on -h argument',()=>{
-
+    it('should show the man page on -h argument', () => {
+        //todo
     });
-    it('compile using the -i argument',()=>{
-
+    it('compile using the -i argument', () => {
+        //todo
     });
 });
 
-async function spawnWithArgs(){
+async function spawnWithArgs() {
 
 
 };
